ceae53dc203175eb29a9e33c0d85cdc7f108de45
Owais Mustafa <owaismustafa57@gmail.com>
2026-08-31 18:32:48 +0100

    Fixed api fetch for wiki

--- backend/src/services/wikiArabic.js
+++ backend/src/services/wikiArabic.js
@@ -1,12 +1,10 @@
 
 async function grabArabic(){
-    const response = await fetch("https://ar.wikipedia.org/w/api.php?action=query&prop=extracts&titles=%D8%AA%D8%A7%D8%B1%D9%8A%D8%AE&format=json&explaintext=true"); // need to get individual sentences
-
-    if(!response.ok){
-        throw new error("could not find resource");
-    }
-    const arabic = response.json();
-    console.log(arabic);
-
+    const resp = await fetch("https://ar.wikipedia.org/w/api.php?action=query&prop=extracts&titles=%D8%AA%D8%A7%D8%B1%D9%8A%D8%AE&format=json&explaintext=true")
+        .then((response)=> {
+            return response.json();
+        })
+    console.log(await resp);
 }
+
 grabArabic();
