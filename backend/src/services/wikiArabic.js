d6844ecc87a1ea0ac6690d82012d7d97470ffc78
Owais Mustafa <owaismustafa57@gmail.com>
2026-08-31 18:32:59 +0100

    extracted content from JSON data

--- backend/src/services/wikiArabic.js
+++ backend/src/services/wikiArabic.js
@@ -1,5 +1,5 @@
 
-async function grabArabic(){
+async function fetchWikiArabic(){
     const resp = await fetch("https://ar.wikipedia.org/w/api.php?action=query&prop=extracts&titles=%D8%AA%D8%A7%D8%B1%D9%8A%D8%AE&format=json&explaintext=true")
         .then((response)=> {
             return response.json(); //this tears open the envelope so we can get the content info
@@ -8,12 +8,25 @@ async function grabArabic(){
     return await resp;
 }
 
-async function arabicData(){
-    let resp = await grabArabic();
-    console.log(Object.valueOf(resp));
-    console.log(Object.values(resp.query));
+async function getArabicData(){
+    let resp = await fetchWikiArabic();
+
+    let obj = Object.keys(resp.query.pages);
+
+    return resp.query.pages[obj[0].toString()].extract;
+
+}
+
+async function splitJSONData(){
+
+   let resp = await getArabicData();
+
+   console.log(resp);
+
+
+
 
 
 }
 
-arabicData();
+splitJSONData();
