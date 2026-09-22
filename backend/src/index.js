73ba286fd7f48481f4f95ad95e6ebfe2ebed66c4
Owais Mustafa <owaismustafa57@gmail.com>
2026-09-22 20:01:26 +0100

    cors changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import sentenceRoutes from './routes/sentenceRoutes.js'
+import cors from 'cors';
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+app.use(cors({origin: 'file:///Users/owaismustafa/Arabic%20Learning%20Project/frontend/index.html'}))
 app.use('/api', sentenceRoutes);
 
 app.get('/', (req, res) => {
